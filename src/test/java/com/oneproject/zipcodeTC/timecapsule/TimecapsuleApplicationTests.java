b35ef5acacbc0e821a83cfd418801b45ae762294
Liu <playerone@MacBook-Air.local>
2022-11-27 10:40:41 -0500

    init

---
+++ src/test/java/com/oneproject/zipcodeTC/timecapsule/TimecapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneproject.zipcodeTC.timecapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
